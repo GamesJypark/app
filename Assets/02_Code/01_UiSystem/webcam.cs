7326b7bffab8f6e3183b797fec016d2563e675a2
laeswo <129704273+laeswo@users.noreply.github.com>
2024-12-22 01:46:42 +0900

    d
    d

--- Assets/02_Code/01_UiSystem/webcam.cs
+++ Assets/02_Code/01_UiSystem/webcam.cs
@@ -9,7 +9,7 @@ public class webcam : MonoBehaviour
        public RawImage webcamDisplay;
        public bool IsHit = false;
        private int i = 0;
-    void Start()
+    void Awake()
     {
         
         WebCamDevice[] devices = WebCamTexture.devices;
@@ -17,6 +17,10 @@ public class webcam : MonoBehaviour
                 {
                     Debug.Log("카메라가 감지되지않았습니다.");
                     return;
+                }
+                else if(devices.Length>=1)
+                {
+                    
                 }
         
                 WebCamTexture webcamTexture = new WebCamTexture(devices[0].name);
@@ -26,58 +30,16 @@ public class webcam : MonoBehaviour
                 webcamTexture.Play();
        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-
-        if (IsHit)
-        {
-            zerotored();
-            IsHit = false;
-        }
+        
         
     }
 
-    public void zerotored()
-    {
-     while (webcamDisplay.color.g > 179 && webcamDisplay.color.b > 179)
-     {
-         webcamDisplay.color = new Color(webcamDisplay.color.r, webcamDisplay.color.g - 0.002f, webcamDisplay.color.b-0.002f,webcamDisplay.color.a);
-     }
 
-     i += 1;
-     if (i >= 8)
-     {
-         i = 0;
-       
-     }
-     else
-     {
-         Invoke("redtozero",0.1f);
-     }
-
-    }
-
-    public void redtozero()
-    {
-        Debug.Log("red to  zero" +
-                  "");
-        while (webcamDisplay.color.g < 255 && webcamDisplay.color.b<255)
-        {
-            webcamDisplay.color = new Color(webcamDisplay.color.r, webcamDisplay.color.g + 0.002f, webcamDisplay.color.b+0.002f,webcamDisplay.color.a);
-        }
-        i += 1;
-        if (i >= 8)
-        {
-            i = 0;
-        }
-        else
-        {
-            Invoke("zerotored",0.1f);
-        }
-    
-    }
+   
 
 
 
