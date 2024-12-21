507a16c41fcb8f868b164a9d92c0c1c974214ede
laeswo <129704273+laeswo@users.noreply.github.com>
2024-12-22 04:53:25 +0900

    end
    end

--- Assets/02_Code/01_UiSystem/hpbarscript.cs
+++ Assets/02_Code/01_UiSystem/hpbarscript.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hpbarscript : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class hpbarscript : MonoBehaviour
         one,two,three,four
     }
 
+    public Text hp;
     public hpstatus status;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,24 +28,28 @@ public class hpbarscript : MonoBehaviour
                 hpbar[1].SetActive(false);
                 hpbar[2].SetActive(false);
                 hpbar[3].SetActive(true);
+                hp.text = "hp : 1ms";
                 break;
             case hpstatus.three:
                 hpbar[0].SetActive(false);
                 hpbar[1].SetActive(false);
                 hpbar[2].SetActive(true);
                 hpbar[3].SetActive(false);
+                hp.text = "hp : 26ms";
                 break;
             case hpstatus.two:
                 hpbar[0].SetActive(false);
                 hpbar[1].SetActive(true);
                 hpbar[2].SetActive(false);
                 hpbar[3].SetActive(false);
+                hp.text = "hp : 51ms";
                 break;
             case hpstatus.one:
                 hpbar[0].SetActive(true);
                 hpbar[1].SetActive(false);
                 hpbar[2].SetActive(false);
                 hpbar[3].SetActive(false);
+                hp.text = "hp : 76ms";
                 break;
         }
     }
