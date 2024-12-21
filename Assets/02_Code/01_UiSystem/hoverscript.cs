c267443451582c617bee0cf6e58f5d7d2a4d2671
laeswo <129704273+laeswo@users.noreply.github.com>
2024-12-21 23:03:17 +0900

    Revert "dd"
    This reverts commit 15a07c806a8ed36e2feac4f682f999af329e55a1.

--- Assets/02_Code/01_UiSystem/hoverscript.cs
+++ Assets/02_Code/01_UiSystem/hoverscript.cs
@@ -11,19 +11,16 @@ public class hoverscript : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public float x;
     public float y;
     public int size;
-
-    
     private void Start()
     {
         x = button.rect.width;
         y = button.rect.height;
         size = text.fontSize;
-   
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-       Debug.Log("adad");
+       
         button.sizeDelta = new Vector2(x*1.5f, y*1.5f);
         text.fontSize = (int)(size * 1.5);
     }
