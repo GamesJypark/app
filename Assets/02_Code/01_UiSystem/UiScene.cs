eb2d05f418d3fdb27c76970ba6026167624b6fab
laeswo <129704273+laeswo@users.noreply.github.com>
2024-12-21 19:28:07 +0900

    Update UiScene.cs

--- Assets/02_Code/01_UiSystem/UiScene.cs
+++ Assets/02_Code/01_UiSystem/UiScene.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UiScene : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class UiScene : MonoBehaviour
     public UIStatus uista;
 
     public List<GameObject> pane;
+    public Text playername;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,8 +27,18 @@ public class UiScene : MonoBehaviour
         switch (uista)
         {
             case UIStatus.start:
+                pane[0].SetActive(true);
+                pane[1].SetActive(false);
+                pane[2].SetActive(false);
                 break;
             case UIStatus.play:
+                pane[0].SetActive(false);
+                pane[1].SetActive(true);
+                pane[2].SetActive(true);
+                break;
+            case UIStatus.end:
+                SceneManager.LoadScene("03_EndingScene");
+            break;
         }
     }
 }
