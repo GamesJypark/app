740292a0e4edb549e2ca9319c4e3678d4dd55d49
GamesJypark <jongyubag19@gmail.com>
2024-12-21 17:53:48 +0900

    .
    .

--- Assets/02_Code/01_Game/Chat/Chat.cs
+++ Assets/02_Code/01_Game/Chat/Chat.cs
@@ -8,41 +8,35 @@ public class Chat : MonoBehaviour
     [SerializeField]private int Score;
     [SerializeField]private float Move_Cycle_Max;
     [SerializeField]private float Move_Cycle_Now;
+    [SerializeField] private bool isMoved = false;
     public void Spawn(int Input_Score, float Input_Move_Cycle, Sprite sprite)
     {
         Chat chat1 = Instantiate(gameObject).GetComponent<Chat>();
         chat1.GetComponent<Image>().sprite = sprite;
-        chat1.transform.SetParent(FindFirstObjectByType<Canvas>().transform, false);
+        chat1.transform.SetParent(GameObject.Find("Content").transform, false);
         chat1.Score = Input_Score;
         chat1.Move_Cycle_Max = Input_Move_Cycle;
         chat1.Move_Cycle_Now = Input_Move_Cycle;
+        chat1.isMoved = false;
     }
     private void Start()
     {
-        StartCoroutine(Cycle());
+        
     }
-    IEnumerator Cycle()
+    private void Update()
     {
-        while (true)
+        if (gameObject.GetComponent<RectTransform>().anchoredPosition.y > 490)
         {
-            if (Move_Cycle_Now > 0)
+            Debug.Log("실패!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(gameObject.GetComponent<RectTransform>().anchoredPosition.x, gameObject.GetComponent<RectTransform>().anchoredPosition.y + Time.deltaTime * 100);
+            if(gameObject.GetComponent<RectTransform>().anchoredPosition.y >= -400 && isMoved == false)
             {
-                yield return new WaitForSeconds(1);
-                Move_Cycle_Now -= 1;
-            }
-            else
-            {
-                if (gameObject.GetComponent<RectTransform>().anchoredPosition.y + 90 > 400)
-                {
-                    Debug.Log("실패!");
-                    Destroy(gameObject);
-                }
-                else
-                {
-                    gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(gameObject.GetComponent<RectTransform>().anchoredPosition.x, gameObject.GetComponent<RectTransform>().anchoredPosition.y + 90);
-
-                }
-                Move_Cycle_Now = Move_Cycle_Max;
+                isMoved = true;
+                FindFirstObjectByType<TestSystem>().ChatSpawn();
             }
         }
     }
