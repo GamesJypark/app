b4963cee380d2b477104542e024408f860d2cad8
GamesJypark <jongyubag19@gmail.com>
2024-12-21 16:59:33 +0900

    오류를 잡았어요
    아이좋아

--- Assets/02_Code/01_Game/Chat/Chat.cs
+++ Assets/02_Code/01_Game/Chat/Chat.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chat : MonoBehaviour
@@ -8,11 +9,14 @@ public class Chat : MonoBehaviour
     [SerializeField]private float Move_Cycle_Now;
     public void Spawn(int Input_Score, float Input_Move_Cycle)
     {
-        Chat chat = Instantiate(gameObject, new Vector2(5, -3), Quaternion.identity).GetComponent<Chat>();
-        chat.Score = Input_Score;
-        chat.Move_Cycle_Max = Input_Move_Cycle;
-        chat.Move_Cycle_Now = Input_Move_Cycle;
-        chat.StartCoroutine(Cycle());
+        Chat chat1 = Instantiate(gameObject, new Vector2(5, -3), Quaternion.identity).GetComponent<Chat>();
+        chat1.Score = Input_Score;
+        chat1.Move_Cycle_Max = Input_Move_Cycle;
+        chat1.Move_Cycle_Now = Input_Move_Cycle;
+    }
+    private void Start()
+    {
+        StartCoroutine(Cycle());
     }
     IEnumerator Cycle()
     {
@@ -22,7 +26,6 @@ public class Chat : MonoBehaviour
             {
                 yield return new WaitForSeconds(1);
                 Move_Cycle_Now -= 1;
-                Debug.Log(Move_Cycle_Now);
             }
             else
             {
